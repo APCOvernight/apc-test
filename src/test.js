6a680c8dd927996fcfbffc3839437d09091b784d
Ian Egner <ian.egner@apc-overnight.com>
2018-01-29 15:01:28 +0000

    Fix lint errors

--- src/test.js
+++ src/test.js
@@ -4,18 +4,35 @@
  * Some code to test our test scripts on
  */
 class TestClass {
+  /**
+   * Test
+   * @param {String} name
+   */
   constructor (name) {
     this.name = name
   }
 
+  /**
+   * Print a string backwards
+   * @param  {String} str
+   * @return {String}
+   */
   backwards (str) {
     return str.split('').reverse().join('')
   }
 
+  /**
+   * Print name
+   * @return {String}
+   */
   async printName () {
     return `My name is ${this.name}`
   }
 
+  /**
+   * Print name backwards
+   * @return {String}
+   */
   printNameBackwards () {
     return `My name backwards is ${this.backwards(this.name)}`
   }
